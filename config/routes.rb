1cd8cad47f10c72d556fdaeb1000bd17e5910f70
ishr-sk <kmrttnr1208@gmail.com>
2021-06-10 06:09:22 +0000

    更新：バリデーション未設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
  get 'top' => 'homes#top'
- root :to => 'homes#top'
  
  resources :books
+ 
+ root :to => 'homes#top'
  
 end
